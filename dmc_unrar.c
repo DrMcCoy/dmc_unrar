3cb5d96f50cd3556e67a2b7416ad5abfbbee00d8
Sven Hesse <drmccoy@drmccoy.de>
2020-06-30 00:23:52 +0200

    Move optional int64_t typedef out of the PPMd-only block
    With the previous commit, int64_t is used by other code as well.

--- dmc_unrar.c
+++ dmc_unrar.c
@@ -242,13 +242,13 @@ typedef signed char int8_t
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+typedef signed long long int64_t;
 typedef unsigned long long uint64_t;
 typedef unsigned long long size_t;
 
 #define SIZE_MAX ((size_t) -1)
 
 /* Only used by the PPMd decoder, so could be left out with PPMd disabled. */
-typedef signed long long int64_t;
 typedef uint64_t uintptr_t;
 #endif
 
